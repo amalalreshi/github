bc803cb2e9a3666990711bdbc9fdec8868102717
amalalreshi <amal.alreshi@gmail.com>
2023-12-11 10:31:25 +0300

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Amal Mohammad Alreshi on 11/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
